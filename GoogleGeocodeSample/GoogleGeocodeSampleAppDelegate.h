165db6b8460169fdd4c8a37f48e844a73fa31f74
PJ Gray <pjgray@saygoodnight.com>
2011-07-28 08:07:29 -0400

    Initial Commit

---
+++ GoogleGeocodeSample/GoogleGeocodeSampleAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  GoogleGeocodeSampleAppDelegate.h
+//  GoogleGeocodeSample
+//
+//  Created by PJ Gray on 7/28/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class GoogleGeocodeSampleViewController;
+
+@interface GoogleGeocodeSampleAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet GoogleGeocodeSampleViewController *viewController;
+
+@end
---
+++ GoogleGeocodeSample/GoogleGeocodeSampleViewController.h
@@ -0,0 +1,13 @@
+//
+//  GoogleGeocodeSampleViewController.h
+//  GoogleGeocodeSample
+//
+//  Created by PJ Gray on 7/28/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GoogleGeocodeSampleViewController : UIViewController
+
+@end
